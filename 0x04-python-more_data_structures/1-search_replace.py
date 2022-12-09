3af31cc02b3660a9c2060cda9de1ebbdec4d8ac2
Eruallah <abdullaholayiwola5@gmail.com>
2022-12-08 17:38:38 -0800

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+def search_replace(my_list, search, replace):
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
